60107a4208d5c5025be12fd2411462563de97530
Souvik Jha <jhasouvik4@gmail.com>
2023-02-07 11:12:14 +0530

    changes

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -87,7 +87,7 @@ function ExpenseForm(props) {
                     <label>Date</label>
                     <input type='date' 
                     value={enteredDate} 
-                    min="2020-01-01" 
+                    min="2019-01-01" 
                     max="2022-12-31" 
                     onChange={dateChangeHandler} />
                 </div>
